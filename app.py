da19c5cbb70d80f6cbeeb0f035e8b3428b0e115a
B._.0721 <rhehd721@naver.com>
2020-10-30 15:40:50 +0900

    수정

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import  SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))  # 내 현재경로
+dbfile = os.path.join(basedir, "db.sqlite")     # 데이터베이스 경로
+
+print(basedir)
+print(dbfile)
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True      # 누군가가 정보를 요청하면 커밋을 하겠다.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Test(db.Model):
+    __tablename__ = 'test_table'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(32), unique = True)
+
+db.create_all()
 
 @app.route('/')
 def hello():
--- app2.py
+++ app2.py
@@ -10,9 +10,9 @@ print(dbfile)
 
 app = Flask(__name__)
 
-app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///'+dbfile
-app.config['SQLAlCHEMY_COMMIT_ON_TEARDOWN'] = True      # 누군가가 정보를 요청하면 커밋을 하겠다.
-app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True      # 누군가가 정보를 요청하면 커밋을 하겠다.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
